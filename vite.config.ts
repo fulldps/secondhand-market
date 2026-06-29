2791d04182f2ead05571c4c25e2fa6a86f636461
dbtu <nazarhayidov@gmail.com>
2026-06-29 18:13:02 +0300

    feat: set up tailwind and shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
 import tanstackRouter from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react()],
+  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
