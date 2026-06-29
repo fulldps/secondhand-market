03218f4c66610f38426787a0645b07cb9a1a3212
Nazar Sinyutin <128363898+fulldps@users.noreply.github.com>
2026-06-29 17:39:51 +0300

    feat: set up routing and data-fetching foundation (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
